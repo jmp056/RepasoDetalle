df6f7c911f9a503daf9537f2bf5ea7ba262b2b2e
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-06-24 19:32:26 -0400

    Creando el Unit test

---
+++ RepasoDetalleTests/AriticulosTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace entidades.Tests
+{
+    [TestClass()]
+    public class AriticulosTests
+    {
+        [TestMethod()]
+        public void AriticulosTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
