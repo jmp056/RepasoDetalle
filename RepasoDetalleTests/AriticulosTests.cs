8edda1df787ae4b9d006e61cfc4961db06002542
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-06-24 19:46:13 -0400

    Terminando el test del metodo modificar a Articulos

--- RepasoDetalleTests/AriticulosTests.cs
+++ RepasoDetalleTests/AriticulosTests.cs
@@ -12,6 +12,32 @@ namespace Entidades.Tests
     [TestClass()]
     public class AriticulosTests
     {
-        
+        [TestMethod()]
+        public void Guardar()
+        {
+            RepositorioBase<Articulos> repositorio;
+            repositorio = new RepositorioBase<Articulos>();
+            Articulos articulo = new Articulos();
+            articulo.ArticuloId = 1;
+            articulo.Descripcion = "Intel Core I5 5tg Gen";
+            articulo.Existencia = 1;
+            articulo.Precio = 1;
+            Assert.IsTrue(repositorio.Guardar(articulo));
+        }
+
+        [TestMethod()]
+        public void Modificar()
+        {
+            RepositorioBase<Articulos> repositorio;
+            repositorio = new RepositorioBase<Articulos>();
+            Articulos articulo = new Articulos();
+            articulo.ArticuloId = 1;
+            articulo.Descripcion = "Intel Core I5 7tg Gen";
+            articulo.Existencia = 1;
+            articulo.Precio = 1;
+            Assert.IsTrue(repositorio.Modificar(articulo));
+        }
+
+
     }
 }
