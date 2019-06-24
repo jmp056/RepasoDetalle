c9d200b4888694e1e56a144b85b8913e455b520f
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-06-24 19:24:24 -0400

    Terminando la entidad Pedidos

--- RepasoDetalle/Form1.cs
+++ RepasoDetalle/Form1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Data;
 using System.Drawing;
 using System.Linq;
@@ -35,6 +36,21 @@ namespace entidades
             Descripcion = string.Empty;
             Existencia = 0;
             Precio = 0;
+        }
+    }
+
+    public class Pedidos
+    {
+        [Key]
+        public int PedidoId { get; set; }
+        public DateTime Fecha { get; set; }
+        public double Monto { get; set; }
+
+        public Pedidos()
+        {
+            PedidoId = 0;
+            Fecha = DateTime.Now;
+            Monto = 0;
         }
     }
 }
