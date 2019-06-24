e0152f210cb0c1d45bcbbd6938e42978e3b073d8
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-06-24 19:51:07 -0400

    Terminando el test al metodo guardar de Pedidos

---
+++ RepasoDetalleTests/PedidosTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BLL;
+
+namespace Entidades.Tests
+{
+    [TestClass()]
+    public class PedidosTests
+    {
+        [TestMethod()]
+        public void Guardar()
+        {
+            RepositorioBase<Pedidos> repositorio;
+            repositorio = new RepositorioBase<Pedidos>();
+            Pedidos pedido = new Pedidos();
+            pedido.PedidoId = 1;
+            pedido.Fecha = DateTime.Now;
+            pedido.Monto = 2;
+            Assert.IsTrue(repositorio.Guardar(pedido));
+        }
+    }
+}
